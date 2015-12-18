6a829e28f947de12acfb401d4355e087bd339c2f
Tory Burgett <vdburgett@gmail.com>
2015-12-18 11:28:37 -0500

    Creating object on index page that contains all artist names, created another object that contains an artist and thier first song

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -1,3 +1,4 @@
+/* global $ */
 "use strict";
 
 (function() {
@@ -6,9 +7,31 @@
     .controller("SongIndexController", [
       "SongFactory",
       SongIndexControllerFunction
-    ])
+    ]);
 
   function SongIndexControllerFunction(SongFactory) {
-    this.songs = SongFactory.query();
+    var self = this;
+    this.songs = SongFactory.query(function(allSongs){
+      self.artists = [];
+      self.artistsData = [];
+      for(var i = 0; i < allSongs.length; i++){
+        var selectedSong = allSongs[i];
+        var newArtist = allSongs[i].artist;
+        // var artistNum = $.inArray(newArtist, self.artists);
+        // var artistNum = jQuery.inArray(newArtist, self.artists);
+        var artistNum = self.artists.indexOf(newArtist);
+        if (artistNum === -1){
+        // if (newArtist in self.artists){
+          var artistSongData = {"artist": newArtist, "artistSongs": []};
+          artistSongData.artistSongs.push(selectedSong);
+          self.artistsData.push(artistSongData);
+          self.artists.push(newArtist);
+        }
+        // if this data (artist name) already exsists within this array then... do not create a new object within the artist array
+      }
+      console.log(self.artists);
+      console.log("now for the big reveal....");
+      console.log(self.artistsData);
+    });
   }
 })();
